6e811df47f878c3d3a6e31336276a439456f51df
neuecc <ils@neue.cc>
2013-11-21 15:49:01 -0800

    .vsdoc to obsolete

--- internal/app.js
+++ internal/app.js
@@ -2,14 +2,25 @@
 var Enumerable = require('../linq.js');
 
 
-Enumerable.range(1, 10).forEach(function(x){
-    console.log(x);
-});
+
+var List = function(){
+}
+List.prototype = new Array(); 
+Enumerable.Utils.extendTo(List);
 
 
 
 
+var list = new List();
 
+list.push(100);
+list.push(200);
+list.push(300);
+list.push(400);
+list.push(500);
+var x = list[0];
 
+var z = list.last();
+console.log(list.last());
 
 var _ = 1;
--- test/equalityComparer.js
+++ test/equalityComparer.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="qunit.js"/>
+/// <reference path="../linq.js" />
+/// <reference path="../extensions/linq.qunit.js" />
+
+module("EqualityComparer");
+
+test("defaultComparer", function () {
+    // var comparer = Enumerable.Utils.getDefaultEqualityComparer();
+
+    //comparer.equals(1, 1).isTrue();
+
+    // comparer.equals("hoge", "huga").isTrue();
+
+    
+
+});
